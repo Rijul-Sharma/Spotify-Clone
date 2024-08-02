e5e284ec876652f2ab39449df2c3d545b9532772
Rijul-Sharma <rijul.sharma117@gmail.com>
2024-08-02 20:38:52 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -119,7 +119,7 @@ async function main(){
 
     //Get list of all songs
     console.log(window.location.pathname);
-    await getSongs('songs/ncs');
+    await getSongs('../songs/ncs');
     playMusic(songs[0],true);
 
 
