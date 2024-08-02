3f9c0333bb661089b11f2bd70fc7fb0b14f73a23
Rijul-Sharma <rijul.sharma117@gmail.com>
2024-08-02 19:48:55 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -118,7 +118,7 @@ async function displayAlbums(){
 async function main(){
 
     //Get list of all songs
-    await getSongs('songs/ANIMAL');
+    await getSongs('songs/Animal');
     playMusic(songs[0],true);
 
 
